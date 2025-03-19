08f9042904d3ff6de1fa5c6b82e6effc410dd3f1
Chandan U <chandanucodeid04@gmail.com>
2025-03-19 21:11:49 +0530

    Update query4.sql

--- prog2/query4.sql
+++ prog2/query4.sql
@@ -1,1 +1,10 @@
+CREATE VIEW Highest_Order_Salesman AS  
+SELECT O.Ord_Date, S.Salesman_id, S.Name, O.Customer_id, O.Purchase_Amt  
+FROM ORDERS O  
+JOIN SALESMAN S ON O.Salesman_id = S.Salesman_id  
+WHERE O.Purchase_Amt = (  
+    SELECT MAX(Purchase_Amt)  
+    FROM ORDERS  
+    WHERE Ord_Date = O.Ord_Date  
+);
 
