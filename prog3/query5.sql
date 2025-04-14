6c7801fac71c7e29ab423675f67fdd775e8519a4
Chandan U <chandanucodeid04@gmail.com>
2025-04-14 20:46:32 +0530

    Update query5.sql

--- prog3/query5.sql
+++ prog3/query5.sql
@@ -1,14 +1,9 @@
-UPDATE RATING
-SET Rev_Stars = 5
-WHERE Mov_id IN (
-    SELECT Mov_id 
-    FROM MOVIES 
-    WHERE Dir_id = (
-        SELECT Dir_id 
-        FROM DIRECTOR 
-        WHERE Dir_Name = 'Steven Spielberg'
-    )
-);
+UPDATE RATING R
+JOIN MOVIES M ON R.Mov_id = M.Mov_id
+JOIN DIRECTOR D ON M.Dir_id = D.Dir_id
+SET R.Rev_Stars = 5
+WHERE D.Dir_Name = 'Steven Spielberg';
+
 
 -- note
 UPDATE table_name
