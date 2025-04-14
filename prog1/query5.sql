02f7be570f83692b286188a4e48a1ca198aed4bf
Chandan U <chandanucodeid04@gmail.com>
2025-04-14 18:17:29 +0530

    Update query5.sql

--- prog1/query5.sql
+++ prog1/query5.sql
@@ -1,9 +1,12 @@
-CREATE VIEW available_books AS
-SELECT bc.book_id, SUM(bc.no_of_copies) AS available_copies
+SELECT 
+    bc.book_id,
+    b.title,
+    bc.no_of_copies - IFNULL(l.lended, 0) AS available_copies
 FROM book_copies bc
-WHERE NOT EXISTS (
-    SELECT 1 FROM book_lending bl
-    WHERE bc.book_id = bl.book_id
-    AND bc.branch_id = bl.branch_id
-)
-GROUP BY bc.book_id;
+JOIN book b ON bc.book_id = b.book_id
+LEFT JOIN (
+    SELECT book_id, branch_id, COUNT(*) AS lended
+    FROM book_lending
+    GROUP BY book_id, branch_id
+) l ON bc.book_id = l.book_id AND bc.branch_id = l.branch_id
+WHERE (bc.no_of_copies - IFNULL(l.lended, 0)) > 0;
