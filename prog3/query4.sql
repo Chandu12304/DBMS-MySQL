dd31aa6b1d051d5e6735fe3a8efd1cda51d30787
Chandan U <chandanucodeid04@gmail.com>
2025-04-14 20:43:56 +0530

    Update query4.sql

--- prog3/query4.sql
+++ prog3/query4.sql
@@ -1,5 +1,12 @@
-SELECT M.Mov_Title, MAX(R.Rev_Stars) AS Highest_Rating
+SELECT m.mov_title, r.rev_stars
+FROM movies m
+JOIN rating r
+ON m.mov_id=r.mov_id
+ORDER BY m.mov_title;
+
+SELECT M.Mov_Title, R.Rev_Stars AS max_rating
 FROM MOVIES M
 JOIN RATING R ON M.Mov_id = R.Mov_id
-GROUP BY M.Mov_Title
-ORDER BY M.Mov_Title;
+WHERE R.Rev_Stars = (
+    SELECT MAX(Rev_Stars) FROM RATING
+);
