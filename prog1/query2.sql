78f8ff2925ba1b314413074cc7f957f0999da3b7
Chandan U <chandanucodeid04@gmail.com>
2025-04-14 17:23:26 +0530

    Update query2.sql

--- prog1/query2.sql
+++ prog1/query2.sql
@@ -1,5 +1,5 @@
-SELECT card_no, COUNT(book_id) AS total_books_borrowed
+SELECT card_no, COUNT(*) AS total_books_borrowed
 FROM book_lending
 WHERE date_out BETWEEN '2011-01-01' AND '2019-06-30'
 GROUP BY card_no
-HAVING COUNT(book_id) > 3;
+HAVING COUNT(*) > 3;
