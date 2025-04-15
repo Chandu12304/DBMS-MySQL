e502984e77bf73cdbf979a7404cdc97d69658840
Chandan U <chandanucodeid04@gmail.com>
2025-04-15 19:33:43 +0530

    Update query5.sql

--- prog5/query5.sql
+++ prog5/query5.sql
@@ -1,10 +1,6 @@
-SELECT DNo, COUNT(*) AS High_Salary_Employees
-FROM EMPLOYEE
-WHERE Salary > 600000
-GROUP BY DNo
-HAVING DNo IN (
-    SELECT DNo 
-    FROM EMPLOYEE 
-    GROUP BY DNo 
-    HAVING COUNT(*) > 5
-);
+SELECT d.dno,COUNT(*) AS High_Salary_Employees
+FROM department d
+JOIN employee e ON e.dno=d.dno
+WHERE e.salary > 60000
+GROUP BY (d.dno)
+HAVING COUNT(*)>0;
